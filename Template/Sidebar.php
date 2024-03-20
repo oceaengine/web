fa530cc1604ad2e8b7bbde709122fa46166fa5ed
oceaengine <riskydinda041@gmail.com>
2024-03-20 11:52:40 +0800

    Tampilan Admin

---
+++ Template/Sidebar.php
@@ -0,0 +1,54 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">POLIBAN</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/avatarjake.jpeg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Azkara Wijaya</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+               
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon far fa-calendar-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="Mahasiswa.php" class="nav-link">
+              <i class="nav-icon far fa-calendar-alt"></i>
+              <p>
+                Data Mahasiswa
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="Prodi.php" class="nav-link">
+              <i class="nav-icon far fa-image"></i>
+              <p>
+                Data Prodi
+              </p>
+            </a>
+          </li>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
